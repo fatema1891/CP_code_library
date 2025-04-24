bb1a53b65b9815002f54b1aa40fffe8c96995ac0
FatemaAkther <fatemacse02@gmail.com>
2025-04-24 19:56:32 +0600

    Add files via upload

---
+++ Basics/binary_search_problem_CSES.cpp
@@ -0,0 +1,40 @@
+ 
+ // CSES problem https://cses.fi/problemset/task/1620
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+using vl=vector<ll>;
+using vvl=vector<vl>;
+const ll mod= 1e9+7;
+#define mx 1e9+7
+
+int32_t main()
+{
+	fast;
+	ll n,t,k,q,i,x;
+     cin>>n>>t;
+     ll a[n];
+
+     for(i=0;i<n;i++)
+     {
+          cin>>a[i];
+     }
+     ll l=0,r=1e18,mid,ans=0;
+     while(l<=r)
+     {
+          mid=l+(r-l)/2;
+          ll sum=0;
+          for(i=0;i<n;i++)
+          {
+               sum+=mid/a[i];
+               if(sum>=t){break;}
+          }
+          if(sum>=t)ans=mid,r=mid-1;
+          else l=mid+1;
+     }
+     cout<<ans<<'\n';
+    
+}
+     
