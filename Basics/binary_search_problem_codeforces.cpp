5c7b173483c84572041797b5ebf8794956ac5eb8
FatemaAkther <fatemacse02@gmail.com>
2025-04-24 23:12:06 +0600

    Add files via upload

---
+++ Basics/binary_search_problem_codeforces.cpp
@@ -0,0 +1,38 @@
+
+//https://codeforces.edu (ITMO Academy) A. Packing Rectangles
+
+#include<bits/stdc++.h>
+using namespace std;
+//#define ll long long int
+using ll =long long int;
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+using vl=vector<ll>;
+using vvl=vector<vl>;
+const ll mod= 1e9+7;
+#define mx 1e9+7
+bool product(ll w,ll h,ll n,ll x)
+{
+     ll rec1=x/w;
+     ll rec2=x/h;
+     ll c=rec1*rec2;
+     return c>=n;
+}
+
+int32_t main()
+{
+	fast;
+	ll n,w,h,i,x;
+     cin>>w>>h>>n;
+     ll l=0,r=1,mid,ans=r;
+     while(product(w,h,n,r)==false) r*=2;
+
+     while(l<=r)
+     {
+          mid=l+(r-l)/2;
+           if(product(w,h,n,mid))ans=mid,r=mid-1;
+            else l=mid+1;
+     }
+     cout<<ans<<'\n';
+    
+}
+     
