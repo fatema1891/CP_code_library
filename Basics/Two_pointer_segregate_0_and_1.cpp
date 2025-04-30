2f6828f61f313c4c37f9e528d2e8a495401cd332
FatemaAkther <fatemacse02@gmail.com>
2025-04-30 09:23:40 +0600

    Add files via upload

---
+++ Basics/Two_pointer_segregate_0_and_1.cpp
@@ -0,0 +1,32 @@
+
+//Educational Codeforces Round 93 (Rated for Div. 2)  C. Good Subarrays
+
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fast ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+int32_t main() {
+    fast;
+    ll i,n,t,j;
+    cin>>n;
+    ll a[n+5];
+    for(i=0;i<n;i++)cin>>a[i];//array element only 0 and 1;
+    //sorting process    //segregate 1 and 0
+    // sort(a,a+n); time complexity:O(NlogN)
+
+    //using two pointer  
+    ll l=0,r=n-1;
+    while(l<r)       //time complexity:O(N)
+    {
+        if(a[l]==0)
+            l++;
+        else
+        {
+            if(a[r]==0)swap(a[l],a[r]),l++,r--;
+            else r--;
+        }
+    }
+    for(i=0;i<n;i++)cout<<a[i]<<" ";
+        //O(NlogN)>O(N)
+}
