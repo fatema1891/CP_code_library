18a9e1e9d3fb22ecf3f1518d9997052d7a6f0873
FatemaAkther <fatemacse02@gmail.com>
2025-09-14 20:15:28 +0600

    Add files via upload

---
+++ Basics/maximum_sum(leetcode_contest_467).cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> maxKDistinct(vector<int>& num, int k) {
+        set<int>st(num.begin(),num.end());
+        vector<int>arr(st.begin(),st.end());
+        long long int n,i,sum=0;
+        n=arr.size();
+        if(k>=n)
+        {
+            sort(arr.rbegin(),arr.rend());
+            return arr;
+        }
+        vector<long long int>pref(n+1,0);
+        for(i=0;i<n;i++)
+            {
+                pref[i+1]=arr[i]+pref[i];
+            }
+        long long int mx=INT_MIN,l;
+        for(i=0;i<=n-k;i++)
+            {
+                sum=pref[i+k]-pref[i];
+                if(sum>mx)
+                {
+                    l=i;
+                    mx=sum;
+                }
+            }
+        vector<int>ans;
+        for(i=l;i<l+k;i++)
+            {
+                ans.push_back(arr[i]);
+            }
+        sort(ans.rbegin(),ans.rend());
+        return ans;
+    }
+};
